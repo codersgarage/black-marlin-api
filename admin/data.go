3c263e8e8e09b4917f0dc84d1aa83209b150234e
Anisuzzaman Khan <aniskhan001@outlook.com>
2018-11-11 00:38:12 +0600

    simplified data naming

--- admin/data.go
+++ admin/data.go
@@ -4,24 +4,24 @@ import (
 	"github.com/codersgarage/black-marlin-api/app"
 )
 
-type AdminDao struct {
+type Data struct {
 }
 
-func NewAdminDao() *AdminDao {
-	return &AdminDao{}
+func NewData() *Data {
+	return &Data{}
 }
 
-func (hd *AdminDao) Save(scope *app.Scope, admin *Admin) error {
+func (hd *Data) Save(scope *app.Scope, admin *Admin) error {
 	return scope.DB.Table(admin.TableName()).Create(admin).Error
 }
 
-func (hd *AdminDao) Get(scope *app.Scope, UUID string, v *Admin) error {
+func (hd *Data) Get(scope *app.Scope, UUID string, v *Admin) error {
 	return scope.DB.
 		Table(v.TableName()).
 		Find(v, "uuid = ?", UUID).Error
 }
 
-func (hd *AdminDao) List(scope *app.Scope, v interface{}) error {
+func (hd *Data) List(scope *app.Scope, v interface{}) error {
 	admin := Admin{}
 	return scope.DB.
 		Table(admin.TableName()).
--- admin/repo.go
+++ admin/repo.go
@@ -11,12 +11,12 @@ import (
 )
 
 type Repo struct {
-	Dao *AdminDao
+	data *Data
 }
 
 func NewRepo() *Repo {
 	return &Repo{
-		Dao: NewAdminDao(),
+		data: NewData(),
 	}
 }
 
@@ -32,7 +32,7 @@ func (hr *Repo) Save(s *app.Scope) (*Admin, error) {
 	admin.CreatedAt = time.Now()
 	admin.UpdatedAt = time.Now()
 
-	if err := hr.Dao.Save(s, admin); err != nil {
+	if err := hr.data.Save(s, admin); err != nil {
 		return nil, errors.NewAPIError(http.StatusInternalServerError, "500001", "Failed to save admin", err)
 	}
 	return admin, nil
@@ -42,7 +42,7 @@ func (hr *Repo) Get(s *app.Scope) (*Admin, error) {
 	admin := &Admin{}
 
 	ID := chi.URLParam(s.Request, "id")
-	err := hr.Dao.Get(s, ID, admin)
+	err := hr.data.Get(s, ID, admin)
 
 	if err != nil && errors.IsRecordNotFoundError(err) {
 		return nil, errors.NewAPIError(http.StatusNotFound, "404001", "admin not found", err)
@@ -59,7 +59,7 @@ func (hr *Repo) Update(s *app.Scope) (*Admin, error) {
 
 func (hr *Repo) List(s *app.Scope) ([]Admin, error) {
 	var data []Admin
-	if err := hr.Dao.List(s, &data); err != nil {
+	if err := hr.data.List(s, &data); err != nil {
 		return nil, errors.NewAPIError(http.StatusInternalServerError, "500001", "Failed to list Admins", err)
 	}
 	if len(data) > 0 {
